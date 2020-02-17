f704159edfb63a3c93bc72dfc5f03be82a6d929c
Evan Miller <evanmiller067@gmail.com>
2020-02-16 21:51:05 -0800

    Still has bugs, not summing correctly/occasional seg fault

--- asgmt03/a03p.c
+++ asgmt03/a03p.c
@@ -8,35 +8,73 @@
 // Parent process prints sum of numbers.
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>         // for fork()
+#include <sys/types.h>      // for pid_t
+#include <sys/wait.h>       // for waitpid()
 
 int main(int argc, char **argv)
 {
-	// set up pipe
+	int fd[2]; //set up fork
+	pid_t pid;
 
-	// call fork()
+	pipe(fd);
+	pid = fork(); //call fork
 
-	printf("CS201 - Assignment 3 Premium - I. Forgot\n");
+	printf("CS201 - Assignment 3 Premium - Evan Miller\n");
 
-	if (0 /* replace 0 with test for parent vs child, delete this comment */) {
+	if (pid == 0) 
+		{
 		// -- running in child process --
-		int     sum = 0;
+		int n, a[100], sum = 0;
 
 		// Receive numbers from parent process via pipe
 		// one at a time, and count them.
 
-		// Return sum of numbers.
-		return sum;
+		read(fd[0], &n, sizeof(n));
+
+		int i;
+		
+		for(i = 0; i < n; i++)
+		{
+			int y;
+
+			read(fd[0], &y, sizeof(y));
+			a[i] = y; // read then store value in array
+		}
+		for(i = 0; i < (n); ++i)
+		{
+			sum += a[i];
+		}
+		write(fd[1], &sum, sizeof(sum)); //sum to parent
+		
+		exit(0);
 		}
 	else {
 		// -- running in parent process --
-		int sum = 0;
+		int a[100], i, sum;
+		
+		if(argc<2)
+		{
+			printf("enter numbers: \n");
+			return 0;
+		}
+		int n = atoi(argv[1]);
+		
+		for(i = 0; i < (n); i++)
+		{
+			a[i] = atoi(argv[i+2]);
+		}
+		write(fd[1], &n, sizeof(n));
+		
+		for(i = 0; i < n; i++)
+		{
+			int x = a[i];
+			write(fd[1], &x, sizeof(x));
+		}
+		wait(NULL);
 
-		// Send numbers (datatype: int, 4 bytes) from command line arguments
-		// starting with argv[1] one at a time through pipe to child process.
-
-		// Wait for child process to return. Reap child process.
-		// Receive sum of numbers via the value returned when
-		// the child process is reaped.
+		read(fd[0], &sum, sizeof(sum));
 
 		printf("sum = %d\n", sum);
 		return 0;
