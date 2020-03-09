c052e097ccbdb423fe600fa38c8710f2fc466837
Evan Miller <evanmiller067@gmail.com>
2020-03-08 20:22:02 -0700

    finished asgmt03

--- asgmt03/a03.c
+++ asgmt03/a03.c
@@ -8,37 +8,58 @@
 // Parent process prints sum of numbers.
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int main(int argc, char **argv)
 {
 	// set up pipe
+	int fd[2];
 
-	// call fork()
+	int x, y, z;
 
-	printf("CS201 - Assignment 3 Regular - I. Forgot\n");
+	pipe(fd);
+	int pid = fork(); // call fork()
 
-	if (0 /* replace 0 with test for parent vs child, delete this comment */) {
+	printf("CS201 - Assignment 3 Regular - Evan Miller\n");
+
+	if (pid == 0) {
 		// -- running in child process --
-		int     sum = 0;
+		int sum = 0;
+		
+		read(fd[0], &x, sizeof(x));
+		read(fd[0], &y, sizeof(y));
+		read(fd[0], &z, sizeof(z));
 
-		// Receive numbers from parent process via pipe
-		// one at a time, and count them.
+		sum = (x+y+z);
+		
+		write(fd[1], &sum, sizeof(sum));
+		close(fd[0]);
+		close(fd[1]);
 
 		// Return sum of numbers.
 		return sum;
 		}
 	else {
 		// -- running in parent process --
-		int sum = 0;
+		int sum = x+y+z;
+		x = atoi(argv[1]);
+		y = atoi(argv[2]);
+		z = atoi(argv[3]);
 
 		// Send numbers (datatype: int, 4 bytes) from command line arguments
 		// starting with argv[1] one at a time through pipe to child process.
-
-		// Wait for child process to return. Reap child process.
-		// Receive sum of numbers via the value returned when
-		// the child process is reaped.
-
+		write(fd[1], &x, sizeof(x));
+		write(fd[1], &y, sizeof(y));
+		write(fd[1], &z, sizeof(z));
+	
+		read(fd[0], &sum, sizeof(sum));
+		
 		printf("sum = %d\n", sum);
-		return 0;
+		
+		close(fd[1]);
+		close(fd[0]);		
 		}
+
+		return 0;
 }
--- asgmt03/a03p.c
+++ asgmt03/a03p.c
@@ -7,76 +7,110 @@
 //
 // Parent process prints sum of numbers.
 
-#include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>         // for fork()
-#include <sys/types.h>      // for pid_t
+#include <stdio.h>
+#include <unistd.h>         // for fork(), sleep(), pipe()
 #include <sys/wait.h>       // for waitpid()
+#include <sys/types.h>		// for pid_t
+static int fd[2];
 
 int main(int argc, char **argv)
 {
-	int fd[2]; //set up fork
+	
 	pid_t pid;
 
-	pipe(fd);
-	pid = fork(); //call fork
+	// set up pipe, with pipe error check
+	if (pipe(fd))
+	{
+		printf("Pipe error\n");
+		return -1;
+	}
+	// call fork(), with error check
+	pid = fork();
+	
+	if(pid < 0)
+	{
+		printf("fork error %d\n", pid);
+		return -1;
+	}
+	
+	//printf("CS201 - Assignment 3 Premium - Evan Miller\n");
 
-	printf("CS201 - Assignment 3 Premium - Evan Miller\n");
-
-	if (pid == 0) 
-		{
+	if (pid == 0) {
 		// -- running in child process --
-		int n, a[100], sum = 0;
+		int sum = 0;	//sum of arg values
+		int argIn = 0;	//to hold input before adding to sum, might not need
+		int signBit = 0; //for handling positive/negative values
 
+		//set up pipes for Child
 		// Receive numbers from parent process via pipe
 		// one at a time, and count them.
 
-		read(fd[0], &n, sizeof(n));
-
-		int i;
-		
-		for(i = 0; i < n; i++)
+		for(int i = 1; i < argc; i++)
 		{
-			int y;
-
-			read(fd[0], &y, sizeof(y));
-			a[i] = y; // read then store value in array
+			read(fd[0], &argIn, 4);
+			sum = sum+argIn;
 		}
-		for(i = 0; i < (n); ++i)
+		//for prem version test for sign, multiply by (-1) to keep neg value with atoi.
+		if(sum < 0)
 		{
-			sum += a[i];
+			signBit = 1; //if sum is a neg value set sign bit to 0
+			sum = (sum *-1);
+			write(fd[1], &signBit, 4); // send signBit value to parent to test
 		}
-		write(fd[1], &sum, sizeof(sum)); //sum to parent
-		
-		exit(0);
+		else//same as above but for positive values
+		{
+			signBit = 0;
+			write(fd[1], &signBit, 4);
+		}
+		close(fd[1]); //close pipe
+
+		// Return sum of numbers.
+		return sum;
 		}
 	else {
 		// -- running in parent process --
-		int a[100], i, sum;
 		
-		if(argc<2)
-		{
-			printf("enter numbers: \n");
-			return 0;
-		}
-		int n = atoi(argv[1]);
+		int sum = 0;
+		int out = 0;
+		int signBit = 0;//default to positive values
 		
-		for(i = 0; i < (n); i++)
-		{
-			a[i] = atoi(argv[i+2]);
-		}
-		write(fd[1], &n, sizeof(n));
+		// Send numbers (datatype: int, 4 bytes) from command line arguments
+		// starting with argv[1] one at a time through pipe to child process.
 		
-		for(i = 0; i < n; i++)
+		for(int i = 1; i < argc; i++)
 		{
-			int x = a[i];
-			write(fd[1], &x, sizeof(x));
+			out = atoi(argv[i]);
+			write(fd[1], &out, 4);
 		}
-		wait(NULL);
+		pid = wait(&sum);//wait for child to send sum, check for signBit
+		read(fd[0], &signBit, 4);
 
-		read(fd[0], &sum, sizeof(sum));
+		close(fd[1]);
 
-		printf("sum = %d\n", sum);
+		// Wait for child process to return. Reap child process.
+		// Receive sum of numbers via the value returned when
+		// the child process is reaped.
+		
+		//shift sum values instead of casting (int)(signed char)sum, should be faster? Due to Negative values?	
+		
+		sum = sum >> 8;
+
+		printf("CS201 - Assignment 3 Premium - Evan Miller\n");	
+		//printf("sum = %d\n", sum);
+
+		//prem version, test for signBit
+
+		if(signBit == 0)
+		{
+			printf("sum = %d\n", sum);
+		}
+		if(signBit == 1)
+		{
+			sum = (sum *-1);
+			printf("sum = %d\n", sum);
+		}
+
 		return 0;
 		}
 }
