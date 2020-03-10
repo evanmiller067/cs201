7d8ca5c9d5257cd11263197f7c0cf3f48720209a
Evan Miller <evanmiller067@gmail.com>
2020-03-10 06:46:11 -0700

    started asgmt 4

--- asgmt04/a04.c
+++ asgmt04/a04.c
@@ -14,29 +14,32 @@ int main(void)
 	int		i;
 
 	// You can add variables between this comment ...
-
+	double total = 0;
+	int j = 0;
 	// ... and this one.
-
+	
 	// Please change 'your name' to your actual name.
-	printf("CS201 - Asgmt 4 - I. Forgot\n");
+	printf("CS201 - Asgmt 4 - Evan Miller\n");
 
 	for (i = 0; i < N_TIMES; i++) {
 
 		// You can change anything between this comment ...
 
-		int     j;
-
+		//int j;
+	//	if(j == 0){
 		for (j = 0; j < ARRAY_SIZE; j++) {
-			sum += array[j];
+			total += array[j];
 			}
-
+		sum = total;
 		// ... and this one. But your inner loop must do the same
 		// number of additions as this one does.
-
 		}
-
+//		else{
+//			sum += total;
+//		}
+//	}
 	// You can add some final code between this comment ...
-
+	printf("sum: %f\n", sum);
 	// ... and this one.
 
 	return 0;
---
+++ asgmt04/a4.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// You are only allowed to make changes to this code as specified by the comments in it.
+
+// The code you submit must have these two values.
+#define N_TIMES     600000
+#define ARRAY_SIZE   10000
+
+int main (void)
+{
+    double  *array = calloc(ARRAY_SIZE, sizeof(double));
+    double  sum = 0;
+    int     i;
+
+    // You can add variables between this comment ...
+    double *ptr;
+    double sum1 = 0;
+    double sum2 = 0;
+    // ... and this one.
+
+    // Please change 'your name' to your actual name.
+    printf("CS201 - Asgmt 4 - Evan Miller\n");
+    for (i = 0; i < N_TIMES; i++) {
+
+    // You can change anything between this comment ...
+        sum1 = 0;
+        sum2 = 0;
+        for (ptr = array; ptr < &array[ARRAY_SIZE]; ptr += 12) {
+            sum1 += (*ptr) + *(ptr + 1) + *(ptr + 2) + *(ptr + 3) + *(ptr + 4) + *(ptr + 5);
+            sum2 += *(ptr + 6) + *(ptr + 7) + *(ptr + 8) + *(ptr + 9) + *(ptr + 10) + *(ptr + 11);
+        }
+        sum += sum1 + sum2;		
+    // ... and this one. But your inner loop must do the same
+    // number of additions as this one does.
+
+    }
+
+    // You can add some final code between this comment ...
+    // ... and this one.
+
+    return 0;
+}
